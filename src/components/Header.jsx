1a82f4530df714fdf10d2190f69b0f6c531e7b68
chloebaisse1 <baissechloe@gmail.com>
2025-03-08 16:27:50 +0400

    creation composant button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,14 @@
 import { logoApp } from "../assets/index"
+import { Button } from "./Button"
 import { Container } from "./Container"
 export const Header = () => {
   return (
     <div className="border-b border-stroke-1">
       <Container className={"bg-n-9"}>
-        <img src={logoApp} width={104} height={32} alt="App_logo" />
-        <button>Login</button>
+        <div className="flex items-center justify-between py-4 lg:py-6">
+          <img src={logoApp} width={104} height={32} alt="App_logo" />
+          <Button theme="primary">Login</Button>
+        </div>
       </Container>
     </div>
   )
