7687991fd8fd0cb60dd29242948aae1a14a3c54a
chloebaisse1 <baissechloe@gmail.com>
2025-03-08 17:32:29 +0400

    section features

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Branding } from "./components/Branding"
+import { Features } from "./components/Features"
 import { Header } from "./components/Header"
 import { Hero } from "./components/Hero"
 
@@ -8,6 +9,7 @@ function App() {
       <Header />
       <Hero />
       <Branding />
+      <Features />
     </>
   )
 }
