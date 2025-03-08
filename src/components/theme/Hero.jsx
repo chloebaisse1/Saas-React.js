c520b357025c88c69b5879bf92dd18511bc722c8
chloebaisse1 <baissechloe@gmail.com>
2025-03-08 16:54:44 +0400

    modifications  section Hero

--- src/components/theme/Hero.jsx
+++ src/components/theme/Hero.jsx
@@ -5,7 +5,7 @@ export const Grid = () => {
     <img
       src={grid}
       alt="grid"
-      className="absolute -top-[1.125rem] left-0 z-0 w-full h-full object-contain object-top pointer-events-none opacity-50"
+      className="hidden lg:block absolute -top-[1.125rem] left-0 z-0 w-full h-full object-contain object-top pointer-events-none opacity-50"
     />
   )
 }
