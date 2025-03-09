691b0d2ede88f4423df750c46eba2457ec67f971
chloebaisse1 <baissechloe@gmail.com>
2025-03-09 15:22:08 +0400

    creation footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Branding } from "./components/Branding"
 import { BulletPoints } from "./components/BulletPoints"
 import { CallToAction } from "./components/CallToAction"
 import { Features } from "./components/Features"
+import { Footer } from "./components/Footer"
 import { Header } from "./components/Header"
 import { Hero } from "./components/Hero"
 import { Princing } from "./components/Princing"
@@ -17,6 +18,7 @@ function App() {
       <BulletPoints />
       <Princing />
       <CallToAction minify />
+      <Footer />
     </>
   )
 }
